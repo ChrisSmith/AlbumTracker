7f4d26f3a76c52d00f57c400c3d97cf9ee70a119
ChrisSmith <chris.smith324@gmail.com>
2012-02-17 21:46:56 -0500

    added bugsense

--- src/org/collegelabs/albumtracker/Application.java
+++ src/org/collegelabs/albumtracker/Application.java
@@ -2,14 +2,19 @@ package org.collegelabs.albumtracker;
 
 import org.collegelabs.library.bitmaploader.BitmapCache;
 
+import com.bugsense.trace.BugSenseHandler;
+
 public class Application extends android.app.Application {
 
 	private BitmapCache mBitmapCache = new BitmapCache();
 
 	public BitmapCache getBitmapCache(){ return mBitmapCache; }
 
+	
 	@Override
 	public void onCreate(){
 		super.onCreate();
+
+		BugSenseHandler.setup(this, Utils.getApiKey(this, Utils.BUG_SENSE_KEY));	
 	}
 }
