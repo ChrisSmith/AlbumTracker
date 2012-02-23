9a6bed7b979c3659c4641cf643d06f52b899e500
ChrisSmith <chris.smith324@gmail.com>
2012-02-22 19:14:26 -0500

    updated account type

--- src/org/collegelabs/albumtracker/Constants.java
+++ src/org/collegelabs/albumtracker/Constants.java
@@ -4,14 +4,10 @@ public class Constants {
 
     /**
      * Account type string.
+     * Also defined in syncadapter.xml and authenticator.xml
      */
-    public static final String ACCOUNT_TYPE = "org.collegelabs.albumtracker.samplesync";
-
-    /**
-     * Authtoken type string.
-     */
-    public static final String AUTHTOKEN_TYPE = "org.collegelabs.albumtracker.samplesync";    
-    
+    public static final String ACCOUNT_TYPE = "org.collegelabs.albumtracker.lastfm_account";
+   
     //Menu Items
 	public static final int MENU_SETTINGS = 1;
 	public static final int MENU_COPY_DB = 2;
