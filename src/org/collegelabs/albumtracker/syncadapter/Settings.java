cd3167af61b0b9f31881b43a3cbe630ea16d8ecc
ChrisSmith <chris.smith324@gmail.com>
2012-02-22 22:01:55 -0500

    about screen

--- src/org/collegelabs/albumtracker/syncadapter/Settings.java
+++ src/org/collegelabs/albumtracker/syncadapter/Settings.java
@@ -2,10 +2,14 @@ package org.collegelabs.albumtracker.syncadapter;
 
 import org.collegelabs.albumtracker.R;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceScreen;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class Settings extends PreferenceActivity {
 
@@ -20,12 +24,22 @@ public class Settings extends PreferenceActivity {
 	@Override
 	public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference){
 		final String title = preference.getTitle().toString();
-		
-		if(title.equals("")){
-		
+		if(title.equals("About")){
+			showAboutDialog();
 		}
 		
 		return super.onPreferenceTreeClick(preferenceScreen, preference);
 	}
 	
+	
+	private void showAboutDialog(){
+		
+		Dialog d = new AlertDialog.Builder(this)
+		.setMessage(R.string.about)
+        .setTitle("About")
+		.setPositiveButton(android.R.string.ok, null)
+		.show();
+		
+		((TextView) d.findViewById(android.R.id.message)).setMovementMethod(LinkMovementMethod.getInstance());
+	}
 }
